79140f3ff33fb776ad4f0df7ff2c9f07c1235db4
Stergiani Kakarali <152175194+StellaKakarali@users.noreply.github.com>
2023-12-14 09:45:21 +0100

    finished task

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // phew… not a lot going on here. Please add some code!
+
+const bookmarkButtons = document.querySelectorAll(
+  '[data-js="bookmark-button"]'
+);
+
+bookmarkButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.classList.toggle("bookmark--active");
+  });
+});
